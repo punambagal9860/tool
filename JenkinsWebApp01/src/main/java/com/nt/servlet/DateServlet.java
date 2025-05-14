e6a4bcc8162e8c8f30f8d8af8173990a66e7ec71
HP <HP@LAPTOP-HA1CV0CM.Dlink>
2025-05-14 19:58:23 +0530

    Jenkins Web App

---
+++ JenkinsWebApp01/src/main/java/com/nt/servlet/DateServlet.java
@@ -0,0 +1,32 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet("/dateurl")  
+public class DateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter pw = res.getWriter();
+
+        Date d = new Date();
+        pw.println("<h1>Date and Time is: " + d + "</h1>");
+        
+        // ✅ Fixed anchor tag
+        pw.println("<br><a href='index.jsp'>Home</a><br>");
+        pw.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        doGet(req, res);
+    }
+}
---
+++ JenkinsWebApp01/src/main/webapp/index.jsp
@@ -0,0 +1,3 @@
+<h1 > Jenkins Web Application</h1>
+<a href="dateurl"> Show Date ANd Time</a>
+
